ad4192cc1b83e430cdd7ee6e2343ea02d381cff5
Angélia Teodosio <2004crt@gmail.com>
2025-09-30 00:33:06 +0200

    c03

---
+++ c03/ex00/ft_strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	s1[] = "qwerty";
+	char	s2[] = "qwerty";
+
+	printf("%d", ft_strcmp(s1, s2));
+	return (0);
+}
